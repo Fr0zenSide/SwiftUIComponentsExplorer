fc792855e6cfa87772bdb888d0a1edcf67ae8949
Jeoffrey Thirot <jeoffrey.thirot@gmail.com>
2024-04-09 08:03:02 +0200

    Initial Commit

---
+++ ViewThatFits/ViewThatFitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewThatFitsApp.swift
+//  ViewThatFits
+//
+//  Created by Jeoffrey Thirot on 09/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewThatFitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
